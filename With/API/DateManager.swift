5427309db93baca5a97eb32b67cb99b100d53041
k-shuya <kwamua@me.com>
2018-05-24 09:38:49 +0900

    各種必要ファイルの追加

---
+++ With/API/DateManager.swift
@@ -0,0 +1,13 @@
+//
+//  DateManager.swift
+//  With
+//
+//  Created by 川村周也 on 2018/05/24.
+//  Copyright © 2018年 川村周也. All rights reserved.
+//
+
+import UIKit
+
+class DateManager: NSObject {
+
+}
---
+++ With/Constraint.swift
@@ -0,0 +1,9 @@
+//
+//  Constraint.swift
+//  With
+//
+//  Created by 川村周也 on 2018/05/24.
+//  Copyright © 2018年 川村周也. All rights reserved.
+//
+
+import Foundation
